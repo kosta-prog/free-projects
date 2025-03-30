e2ffbb8896489bdc756aaf9664d7fd18d4dff4f9
kosta-prog <mirtskhulavakonstantine@gmail.com>
2025-03-30 22:53:21 +0400

    New folder

---
+++ New folder/xyindicator.py
@@ -0,0 +1,17 @@
+from turtle import *
+x = float(input("put x:"))
+y = float(input("put y:"))
+goto (x, y)
+right(1)
+begin_fill()
+forward(1)
+right(1)
+forward(1)
+right(1)
+forward(1)
+right(1)
+forward(1)
+end_fill()
+exitonclick()
+
+
